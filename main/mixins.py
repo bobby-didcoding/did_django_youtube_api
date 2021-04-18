6ecc943580574091ad561d00ee0c3344ac708710
bobby-didcoding <80459294+bobby-didcoding@users.noreply.github.com>
2021-04-18 15:32:19 +0100

    debugged

--- main/mixins.py
+++ main/mixins.py
@@ -6,55 +6,6 @@ import os
 import googleapiclient.discovery
 import googleapiclient.errors
 
-'''
-Handles directions from Google
-'''
-def Directions(*args, **kwargs):
-
-	lat_a = kwargs.get("lat_a")
-	long_a = kwargs.get("long_a")
-	lat_b = kwargs.get("lat_b")
-	long_b = kwargs.get("long_b")
-
-	origin = f'{lat_a},{long_a}'
-	destination = f'{lat_b},{long_b}'
-
-	result = requests.get(
-		'https://maps.googleapis.com/maps/api/directions/json?',
-		 params={
-		 'origin': origin,
-		 'destination': destination,
-		 "key": settings.GOOGLE_API_KEY
-		 })
-
-	directions = result.json()
-
-	if directions["status"] == "OK":
-
-		route = directions["routes"][0]["legs"][0]
-		origin = route["start_address"]
-		destination = route["end_address"]
-		distance = route["distance"]["text"]
-		duration = route["duration"]["text"]
-
-		steps = [
-			[
-				s["distance"]["text"],
-				s["duration"]["text"],
-				s["html_instructions"],
-
-			]
-			for s in route["steps"]]
-
-	return {
-		"origin": origin,
-		"destination": destination,
-		"distance": distance,
-		"duration": duration,
-		"steps": steps
-		}
-
-
 
 
 def title_formatting(title):
@@ -146,7 +97,6 @@ class YouTube:
 					"description":item["snippet"]["description"],
 					"thumbnail":item["snippet"]["thumbnails"]["medium"]["url"],
 					"iframe":item["player"]["embedHtml"],
-					"title":item["snippet"]["title"],
 				}
 
 				data.append(vid_data)
--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-
+#33389
 app_name = "main"
 
 urlpatterns = [
